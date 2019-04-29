e62848fe0c6809e1bf58b3d62f7ca72ae22a0fed
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-29 07:33:59 -0700

    Added duration min,max per set and min,max totals per workout

--- src/training_planner/core.clj
+++ src/training_planner/core.clj
@@ -1,6 +1,8 @@
 (ns training-planner.core
   (:gen-class))
 
+; shoot for M: 80, T: 130, W: 50 Th: 100: F: 0, S: 300, S: 50
+
 (def intensities (hash-map
                    :RI 10
                    :TR 8.5
@@ -8,6 +10,39 @@
                    :ER 5.5
                    :RR 4.5
                    ))
+
+(def duration-min (hash-map
+                  :RI 1
+                  :TR 8
+                  :SSR 20
+                  :ER 1000
+                  :RR 20
+                  ))
+
+(def duration-max (hash-map
+                   :RI 3
+                   :TR 20
+                   :SSR 60
+                   :ER 1000
+                   :RR 60
+                   ))
+
+(def duration-total-min (hash-map
+                   :RI 12
+                   :TR 30
+                   :SSR 30
+                   :ER 1000
+                   :RR 1000
+                   ))
+
+(def duration-total-max (hash-map
+                     :RI 24
+                     :TR 60
+                     :SSR 120
+                     :ER 1000
+                     :RR 1000
+                     ))
+
 
 
 (def intensity-tss (hash-map
