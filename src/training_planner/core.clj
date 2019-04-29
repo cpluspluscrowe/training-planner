2e5d72c09ccc41158a7f9333565eb18161fe1cf6
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-29 06:56:05 -0700

    added basic dicts for different run types

--- src/training_planner/core.clj
+++ src/training_planner/core.clj
@@ -1,7 +1,18 @@
 (ns training-planner.core
   (:gen-class))
 
+(defn TSS [run-type-durations]
+  (let [running-interval (:RI run-type-durations)
+        tempo (:TR run-type-durations)
+        steady-state (:SSR run-type-durations)
+        endurance (:ER run-type-durations)
+        recovery (:RR run-type-durations)
+        ]
+  (println running-interval)
+  ))
+
 (defn -main
   "Creates a training plan"
   [& args]
+  (TSS (hash-map :RI 1 :TR 2 :SSR 3 :ER 4 :RR 5))
   (println "Go run!!"))
