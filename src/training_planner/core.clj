1cb405b4fe1e6491cdc04d04d500d2bd157a85b7
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-29 07:03:50 -0700

    updated with intensity to tss and each intensity per type of run

--- src/training_planner/core.clj
+++ src/training_planner/core.clj
@@ -1,6 +1,28 @@
 (ns training-planner.core
   (:gen-class))
 
+(def intensities (hash-map
+                   :RI 10
+                   :TR 8.5
+                   :SSR 7
+                   :ER 5.5
+                   :RR 4.5
+                   ))
+
+
+(def intensity-tss (hash-map
+                     1 20
+                     2 30
+                     3 40
+                     4 50
+                     5 60
+                     6 70
+                     7 80
+                     8 100
+                     9 120
+                     10 140
+                     ))
+
 (defn TSS [run-type-durations]
   (let [running-interval (:RI run-type-durations)
         tempo (:TR run-type-durations)
