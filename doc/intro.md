7bfd175c4426f0ad031e7de488f11f7c0b6ab9e3
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-29 06:22:24 -0700

    uploading the clojure files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to training-planner
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/training_planner/core.clj
@@ -0,0 +1,7 @@
+(ns training-planner.core
+  (:gen-class))
+
+(defn -main
+  "Creates a training plan"
+  [& args]
+  (println "Go run!!"))
